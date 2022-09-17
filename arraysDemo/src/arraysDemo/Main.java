78ebe1e428ef643596cb945e6491e3a373043b34
Belgin Çoban <belgin_coban@outlook.com>
2022-09-17 18:55:27 +0300

    add homework

---
+++ arraysDemo/src/arraysDemo/Main.java
@@ -0,0 +1,39 @@
+package arraysDemo;
+
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String ogrenci1="Engin";
+		String ogrenci2="Derin";
+		String ogrenci3="Salih";
+		  
+		System.out.println(ogrenci1);
+		System.out.println(ogrenci2);
+		System.out.println(ogrenci3);
+
+		System.out.println("----------------------");
+		//Diziler aynı tipte verileri tutarlar.
+		
+		String[] ogrenciler=new String[4];
+		ogrenciler[0]="Engin";
+		ogrenciler[1]="Derin";
+		ogrenciler[2]="Salih";
+		ogrenciler[3]="Belgin";
+		
+		for (int i = 0; i < ogrenciler.length; i++) {
+			System.out.println(ogrenciler[i]);
+		}
+		System.out.println("****************");
+		
+		//foreach
+		
+		for (String ogrenci : ogrenciler) {
+			System.out.println(ogrenci);
+		}
+
+		//OutOfBounds-sınırların dışında hatası
+}
+}
