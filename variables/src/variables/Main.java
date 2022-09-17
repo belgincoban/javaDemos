a6ef153d07fbec07de5a18bf97b403ef5faab0b3
Belgin Çoban <belgin_coban@outlook.com>
2022-09-17 19:04:36 +0300

    add homework

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Java case sensitive bir dildir.
+		//yani büyük-küçük harf duyarlılığına sahip
+		//degisken tanımlamalarda camelCase yöntemi kullanılır.
+			
+		
+		int sayi=19;
+		String mesaj="Öğrenci sayım: ";
+		System.out.println(mesaj + sayi);
+		System.out.println("Öğrenci sayım: "+sayi);
+		System.out.println("Öğrenci sayım: 19");
+		//verileri değişken olarak tutmak avantajlı
+		//reusability:bir kere yaz onu her yerde kullan.
+		//tekrar tekrar kullanılabilirlik.
+		
+		
+	}
+
+}
