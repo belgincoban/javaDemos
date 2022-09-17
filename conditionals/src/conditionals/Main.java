115b334eba96d8e3216b529e57142f7d83a21156
Belgin Çoban <belgin_coban@outlook.com>
2022-09-17 18:57:31 +0300

    add homework

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,21 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//karar yapıları
+		
+		int sayi =24;
+		if(sayi<20) {
+			System.out.println("sayı 20'den küçüktür.");
+		}
+		else if(sayi==20){
+			System.out.println("sayı 20'e eşittir..");
+		}
+		else {
+			System.out.println("sayi 20'den büyüktür.");
+		}
+
+	}
+
+}
