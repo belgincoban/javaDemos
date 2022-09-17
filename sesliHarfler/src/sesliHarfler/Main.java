59b064bd198028de43ee9817cc2664cf239c42de
Belgin Çoban <belgin_coban@outlook.com>
2022-09-17 19:03:12 +0300

    add homework

---
+++ sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,22 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	    char harf='O';
+	    
+	    switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın sesli harf");
+			break;
+		default:
+			System.out.println("Ince sesli harf");
+		}
+		
+	}
+
+}
