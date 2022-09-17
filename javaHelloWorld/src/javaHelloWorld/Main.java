6a4908f5491c2e196e70521b90886d39f837047a
Belgin Çoban <belgin_coban@outlook.com>
2022-09-17 18:58:52 +0300

    add homework

---
+++ javaHelloWorld/src/javaHelloWorld/Main.java
@@ -0,0 +1,13 @@
+package javaHelloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//yorum satırları
+		
+		System.out.println("hello java");
+		System.out.println("merhaba ilk java projem");
+	}
+	
+}
